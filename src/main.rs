f8a54f590bbe2c16ddad3a96ecaebe5d20872428
Ashish Verma <45747594+vermavashish@users.noreply.github.com>
2024-07-27 15:49:04 +0530

    jwt auth implemented

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-mod routes;
-mod controllers {
-    pub mod tenant_handler;
+mod auth {
+    pub mod auth;
 }
+mod error;
 mod models {
-    pub mod tenant_model;
+    pub mod login_model;
 }
+mod controllers {
+    pub mod login_handler;
+}
+mod routes;
+// mod util;
+use controllers::login_handler::*;
+use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let routes = routes::routes();
-
+    let routes = routes::routes().recover(error::handle_rejection);
     println!("Server started at http://localhost:9000");
     warp::serve(routes).run(([127, 0, 0, 1], 9000)).await;
 }
